c68baaae372b357cbe07627d9a8a7ee79a5524a9
Jesus Fregoso <jfregoso@Jesuss-MacBook-Pro.local>
2025-05-13 13:22:35 -0700

    chore(): fix team coworkers

--- components/Team.vue
+++ components/Team.vue
@@ -17,9 +17,9 @@ interface SocialNetworkProps {
 const teamList: TeamProps[] = [
   {
     imageUrl: "https://i.pravatar.cc/250?img=58",
-    firstName: "Leo",
-    lastName: "Miranda",
-    positions: ["Vue Fronted Developer", "Creator Of This Website"],
+    firstName: "Jesus",
+    lastName: "Fregoso",
+    positions: ["Director At Sunset", "Creator Of This Website", "Visionary and strategist"],
     socialNetworks: [
       {
         name: "LinkedIn",
@@ -38,9 +38,9 @@ const teamList: TeamProps[] = [
   {
     imageUrl:
       "https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    firstName: "David",
-    lastName: "Diaz",
-    positions: ["Machine Learning Engineer", "TensorFlow Tinkerer"],
+    firstName: "Ricardo",
+    lastName: "Osuna",
+    positions: ["Backend Expert", "Web Developer Expert"],
     socialNetworks: [
       {
         name: "LinkedIn",
@@ -55,9 +55,9 @@ const teamList: TeamProps[] = [
   {
     imageUrl:
       "https://images.unsplash.com/photo-1573497161161-c3e73707e25c?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    firstName: "Sarah",
-    lastName: "Robinson",
-    positions: ["Cloud Native Developer", " Kubernetes Orchestrator"],
+    firstName: "Alejandra",
+    lastName: "Moreno",
+    positions: ["UI/UX Designer"],
     socialNetworks: [
       {
         name: "LinkedIn",
@@ -76,9 +76,9 @@ const teamList: TeamProps[] = [
   {
     imageUrl:
       "https://images.unsplash.com/photo-1616805765352-beedbad46b2a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    firstName: "Michael",
-    lastName: "Holland",
-    positions: ["DevOps Engineer", "CI/CD Pipeline Mastermind"],
+    firstName: "Jesus",
+    lastName: "Angulo",
+    positions: ["APP Developer Expert", "Web Developer Expert"],
     socialNetworks: [
       {
         name: "LinkedIn",
